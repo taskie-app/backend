b82df7e0c8bb8cae912e7f70d1725d5b78d3a482
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-11-26 12:27:27 +0700

    UPDATE: user sign up & sign in

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,17 +8,20 @@ const JWT_SECRET = "jwt-secret";
 exports.createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+
     const user = await UserModel.findOne({ email });
     if (user) {
-      return res.status(409).json({ message: "User already existed" });
+      return res.json({ error: "User already existed" });
     }
+
     const hashedPassword = await bcrypt.hash(password, ROUNDS);
     const newUser = new UserModel({ name, email, password: hashedPassword });
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    const token = jwt.sign({ userId: newUser._id }, JWT_SECRET);
+    res.json({ token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.json({ error: "Internal server error" });
   }
 };
 
@@ -27,14 +30,17 @@ exports.signInUser = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await UserModel.findOne({ email });
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      return res.status(401).json({ message: "Invalid credentials" });
+    if (!user) {
+      return res.json({ error: "User not existed" });
     }
-    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.json({ error: "Invalid password" });
+    }
 
-    res.status(200).json({ message: "Valid credentials", token });
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+    res.json({ token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.json({ error: "Internal server error" });
   }
 };
