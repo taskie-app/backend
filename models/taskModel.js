a2bc4004fb7b481888f00bbc2b2d2ecbfd0d5e84
Nguyen Dinh Dat <datnguyendinh1311@gmail.com>
2023-11-26 14:04:40 +0700

    TK-21 CRUD task

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  name: String,
+  projectId: mongoose.Schema.Types.ObjectId,
+  title: String,
+  description: String,
+  assignedTo: {
+    userId: mongoose.Schema.Types.ObjectId,
+    username: String,
+  },
+  status: String,
+  dueDate: Date,
 });
 
 const TaskModel = mongoose.model("Task", taskSchema);
