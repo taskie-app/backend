baf44418dc09036dee99f506626cf5829a7c0728
Van-Dong <dong.nv204724@sis.hust.edu.vn>
2023-11-28 21:41:18 +0700

    ADD: create projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,11 +1,30 @@
-exports.createProject = async (req, res) => {};
+const ProjectModel = require("../models/projectModel");
 
-exports.getProjects = async (req, res) => {};
+exports.createProject = async (req, res) => {
+    const { name, description, manager, members } = req.body;
 
-exports.getProjectDetails = async (req, res) => {};
+    try {
+        const newProject = new ProjectModel({
+            name,
+            description,
+            manager,
+            members,
+        });
+        await newProject.save();
 
-exports.updateProject = async (req, res) => {};
+        res.status(201).json({ data: { project: newProject } });
+    } catch (error) {
+        console.error(error);
+        res.json({ error });
+    }
+};
 
-exports.deleteProject = async (req, res) => {};
+exports.getProjects = async (req, res) => { };
 
-exports.inviteUserToProject = async (req, res) => {};
+exports.getProjectDetails = async (req, res) => { };
+
+exports.updateProject = async (req, res) => { };
+
+exports.deleteProject = async (req, res) => { };
+
+exports.inviteUserToProject = async (req, res) => { };
